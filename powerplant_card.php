d3a7ea2d1a1b9db9562f85b58af6a32d56fd7376
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-12-30 17:42:44 +0100

    Update powerplant_card.php

--- powerplant_card.php
+++ powerplant_card.php
@@ -93,12 +93,7 @@ include_once DOL_DOCUMENT_ROOT.'/core/class/html.form.class.php';
 include_once DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php';
 include_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
 include_once DOL_DOCUMENT_ROOT.'/core/class/html.formprojet.class.php';
-if (!class_exists('FormProduct')) {
-	dol_include_once('/core/class/html.formproduct.class.php');
-}
-if (!class_exists('FormProduct')) {
-	dol_include_once('/product/class/html.formproduct.class.php');
-}
+include_once DOL_DOCUMENT_ROOT.'/product/class/html.formproduct.class.php';
 dol_include_once('/powerplantpv/class/powerplant.class.php');
 dol_include_once('/powerplantpv/lib/powerplantpv_powerplant.lib.php');
 
