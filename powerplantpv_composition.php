e8a28fa421012751f33af250391e4d8c3acfcf5b
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-13 23:15:40 +0200

    refactor(powerplant): align composition tab with Dolibarr list model

---
+++ powerplantpv_composition.php
@@ -0,0 +1,300 @@
+<?php
+/* Copyright (C) 2026		Pierre Ardoin			<developpeur@lesmetiersdubatiment.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file		powerplantpv_composition.php
+ * \ingroup	powerplantpv
+ * \brief	Material composition list tab for power plant
+ */
+
+$res = 0;
+if (!$res && !empty($_SERVER['CONTEXT_DOCUMENT_ROOT'])) {
+	$res = @include str_replace('..', '', $_SERVER['CONTEXT_DOCUMENT_ROOT']).'/main.inc.php';
+}
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--;
+	$j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)).'/main.inc.php')) {
+	$res = @include substr($tmp, 0, ($i + 1)).'/main.inc.php';
+}
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))).'/main.inc.php')) {
+	$res = @include dirname(substr($tmp, 0, ($i + 1))).'/main.inc.php';
+}
+if (!$res && file_exists('../main.inc.php')) {
+	$res = @include '../main.inc.php';
+}
+if (!$res && file_exists('../../main.inc.php')) {
+	$res = @include '../../main.inc.php';
+}
+if (!$res && file_exists('../../../main.inc.php')) {
+	$res = @include '../../../main.inc.php';
+}
+if (!$res) {
+	die('Include of main fails');
+}
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.form.class.php';
+dol_include_once('/powerplantpv/class/powerplant.class.php');
+dol_include_once('/powerplantpv/lib/powerplantpv_powerplant.lib.php');
+
+$langs->loadLangs(array('powerplantpv@powerplantpv', 'products', 'other'));
+
+$id = GETPOSTINT('id');
+$ref = GETPOST('ref', 'alpha');
+$action = GETPOST('action', 'aZ09');
+$lineid = GETPOSTINT('lineid');
+
+$limit = GETPOSTINT('limit') ? GETPOSTINT('limit') : $conf->liste_limit;
+$sortfield = GETPOST('sortfield', 'aZ09comma');
+$sortorder = GETPOST('sortorder', 'aZ09comma');
+$page = GETPOSTISSET('pageplusone') ? (GETPOSTINT('pageplusone') - 1) : GETPOSTINT('page');
+if (empty($page) || $page < 0 || GETPOST('button_search', 'alpha') || GETPOST('button_removefilter', 'alpha')) {
+	$page = 0;
+}
+$offset = $limit * $page;
+
+if (empty($sortfield)) {
+	$sortfield = 'p.ref';
+}
+if (empty($sortorder)) {
+	$sortorder = 'ASC';
+}
+
+$search_ref = trim(GETPOST('search_ref', 'alphanohtml'));
+$search_label = trim(GETPOST('search_label', 'alphanohtml'));
+$search_nature = GETPOSTINT('search_nature');
+$search_serial = trim(GETPOST('search_serial', 'alphanohtml'));
+$search_commissioning = trim(GETPOST('search_commissioning', 'alphanohtml'));
+
+$object = new PowerPlant($db);
+$form = new Form($db);
+$hookmanager->initHooks(array($object->element.'composition', 'globalcard'));
+
+$enablepermissioncheck = getDolGlobalInt('POWERPLANTPV_ENABLE_PERMISSION_CHECK');
+if ($enablepermissioncheck) {
+	$permissiontoread = $user->hasRight('powerplantpv', 'powerplant', 'read');
+	$permissiontoadd = $user->hasRight('powerplantpv', 'powerplant', 'write');
+} else {
+	$permissiontoread = 1;
+	$permissiontoadd = 1;
+}
+
+if (!isModEnabled($object->module) || !$permissiontoread) {
+	accessforbidden();
+}
+
+include DOL_DOCUMENT_ROOT.'/core/actions_fetchobject.inc.php';
+
+$categories = array(
+	50 => $langs->trans('PVModules'),
+	51 => $langs->trans('PVInverters'),
+	52 => $langs->trans('PVIntegration'),
+	53 => $langs->trans('PVMonitoring'),
+	54 => $langs->trans('PVACBox'),
+	55 => $langs->trans('PVDCBox'),
+);
+
+if (GETPOST('button_removefilter', 'alpha')) {
+	$search_ref = '';
+	$search_label = '';
+	$search_nature = 0;
+	$search_serial = '';
+	$search_commissioning = '';
+}
+
+$canedit = ($permissiontoadd && (int) $object->status === (int) $object::STATUS_DRAFT);
+
+if ($action === 'addcomposition' && $canedit) {
+	if (!checkToken()) {
+		accessforbidden();
+	}
+
+	$fk_product = GETPOSTINT('fk_product');
+	$naturecode = GETPOSTINT('naturecode');
+	$serial_number = GETPOST('serial_number', 'alphanohtml');
+	$commissioning_date = dol_mktime(0, 0, 0, GETPOSTINT('commissioning_datemonth'), GETPOSTINT('commissioning_dateday'), GETPOSTINT('commissioning_dateyear'));
+
+	if ($fk_product > 0 && isset($categories[$naturecode])) {
+		$sql = 'INSERT INTO '.$db->prefix()."powerplantpv_powerplantcomp(fk_powerplant, fk_product, nature_code, qty, serial_number, commissioning_date, entity)";
+		$sql .= ' VALUES ('.((int) $object->id).', '.((int) $fk_product).', '.((int) $naturecode).', 1';
+		$sql .= ", '".$db->escape($serial_number)."'";
+		$sql .= ', '.($commissioning_date > 0 ? "'".$db->idate($commissioning_date)."'" : 'NULL');
+		$sql .= ', '.((int) $conf->entity).')';
+		$db->query($sql);
+	}
+}
+
+if ($action === 'delcomposition' && $canedit && $lineid > 0) {
+	if (!checkToken()) {
+		accessforbidden();
+	}
+
+	$sql = 'DELETE FROM '.$db->prefix().'powerplantpv_powerplantcomp';
+	$sql .= ' WHERE rowid = '.((int) $lineid);
+	$sql .= ' AND fk_powerplant = '.((int) $object->id);
+	$sql .= ' AND entity = '.((int) $conf->entity);
+	$db->query($sql);
+}
+
+$sqlwhere = ' WHERE c.fk_powerplant = '.((int) $object->id).' AND c.entity = '.((int) $conf->entity);
+if ($search_ref !== '') {
+	$sqlwhere .= " AND p.ref LIKE '%".$db->escape($search_ref)."%'";
+}
+if ($search_label !== '') {
+	$sqlwhere .= " AND p.label LIKE '%".$db->escape($search_label)."%'";
+}
+if ($search_nature > 0) {
+	$sqlwhere .= ' AND c.nature_code = '.((int) $search_nature);
+}
+if ($search_serial !== '') {
+	$sqlwhere .= " AND c.serial_number LIKE '%".$db->escape($search_serial)."%'";
+}
+if ($search_commissioning !== '') {
+	$sqlwhere .= " AND c.commissioning_date = '".$db->escape($search_commissioning)."'";
+}
+
+$sqlcount = 'SELECT COUNT(c.rowid) as nb';
+$sqlcount .= ' FROM '.$db->prefix().'powerplantpv_powerplantcomp as c';
+$sqlcount .= ' JOIN '.$db->prefix().'product as p ON p.rowid = c.fk_product';
+$sqlcount .= $sqlwhere;
+$rescount = $db->query($sqlcount);
+$nbtotalofrecords = 0;
+if ($rescount) {
+	$objcount = $db->fetch_object($rescount);
+	$nbtotalofrecords = (int) $objcount->nb;
+}
+
+$sql = 'SELECT c.rowid, c.nature_code, c.serial_number, c.commissioning_date, p.ref as product_ref, p.label as product_label';
+$sql .= ' FROM '.$db->prefix().'powerplantpv_powerplantcomp as c';
+$sql .= ' JOIN '.$db->prefix().'product as p ON p.rowid = c.fk_product';
+$sql .= $sqlwhere;
+$sql .= $db->order($sortfield, $sortorder);
+$sql .= $db->plimit($limit + 1, $offset);
+$resql = $db->query($sql);
+
+$title = $langs->trans('PowerPlant').' - '.$langs->trans('PowerPlantMaterialComposition');
+llxHeader('', $title, '');
+
+if ($id > 0 || !empty($ref)) {
+	$object->fetch_thirdparty();
+
+	$head = powerplantPrepareHead($object);
+	print dol_get_fiche_head($head, 'composition', $langs->trans('PowerPlant'), -1, $object->picto);
+
+	$linkback = '<a href="'.dol_buildpath('/powerplantpv/powerplant_list.php', 1).'?restore_lastsearch_values=1">'.$langs->trans('BackToList').'</a>';
+	dol_banner_tab($object, 'ref', $linkback, 1, 'ref', 'ref');
+
+	print '<div class="fichecenter">';
+	print '<div class="underbanner clearboth"></div>';
+
+	$param = 'id='.$object->id;
+	if ($search_ref !== '') {
+		$param .= '&search_ref='.urlencode($search_ref);
+	}
+	if ($search_label !== '') {
+		$param .= '&search_label='.urlencode($search_label);
+	}
+	if ($search_nature > 0) {
+		$param .= '&search_nature='.$search_nature;
+	}
+	if ($search_serial !== '') {
+		$param .= '&search_serial='.urlencode($search_serial);
+	}
+	if ($search_commissioning !== '') {
+		$param .= '&search_commissioning='.urlencode($search_commissioning);
+	}
+
+	$newcardbutton = '';
+	if ($canedit) {
+		$newcardbutton .= '<form method="POST" action="'.$_SERVER['PHP_SELF'].'?id='.$object->id.'">';
+		$newcardbutton .= '<input type="hidden" name="token" value="'.newToken().'">';
+		$newcardbutton .= '<input type="hidden" name="action" value="addcomposition">';
+		$newcardbutton .= '<div class="inline-block">';
+		$newcardbutton .= $form->select_produits(0, 'fk_product', '', 0, 0, -1, 2, '', 0, array(), '', 1, 0);
+		$newcardbutton .= '</div> ';
+		$newcardbutton .= '<div class="inline-block">'.$form->selectarray('naturecode', $categories, 50, 0).'</div> ';
+		$newcardbutton .= '<div class="inline-block"><input type="text" class="flat minwidth100" name="serial_number" placeholder="'.$langs->trans('PowerPlantSerialNumber').'"></div> ';
+		$newcardbutton .= '<div class="inline-block">'.$form->selectDate('', 'commissioning_date', 0, 0, 1, '', 1, 0).'</div> ';
+		$newcardbutton .= '<input type="submit" class="button button-add" value="+">';
+		$newcardbutton .= '</form>';
+	}
+
+	print_barre_liste($langs->trans('PowerPlantMaterialComposition'), $page, $_SERVER['PHP_SELF'], $param, $sortfield, $sortorder, $newcardbutton, $nbtotalofrecords, $nbtotalofrecords, '', 0, '', '', $limit);
+
+	print '<form method="GET" action="'.$_SERVER['PHP_SELF'].'">';
+	print '<input type="hidden" name="id" value="'.$object->id.'">';
+	print '<div class="div-table-responsive">';
+	print '<table class="tagtable liste centpercent">';
+
+	print '<tr class="liste_titre_filter">';
+	print '<td><input type="text" class="flat maxwidth100" name="search_ref" value="'.dol_escape_htmltag($search_ref).'"></td>';
+	print '<td><input type="text" class="flat maxwidth150" name="search_label" value="'.dol_escape_htmltag($search_label).'"></td>';
+	print '<td>'.$form->selectarray('search_nature', array(0 => '') + $categories, $search_nature, 0).'</td>';
+	print '<td><input type="text" class="flat maxwidth100" name="search_serial" value="'.dol_escape_htmltag($search_serial).'"></td>';
+	print '<td><input type="date" class="flat" name="search_commissioning" value="'.dol_escape_htmltag($search_commissioning).'"></td>';
+	print '<td class="right">';
+	print '<button class="button" type="submit" name="button_search" value="search">'.$langs->trans('Search').'</button> ';
+	print '<button class="button" type="submit" name="button_removefilter" value="remove">'.$langs->trans('RemoveFilter').'</button>';
+	print '</td>';
+	print '</tr>';
+
+	print '<tr class="liste_titre">';
+	print_liste_field_titre($langs->trans('Ref'), $_SERVER['PHP_SELF'], 'p.ref', '', $param, '', $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans('Label'), $_SERVER['PHP_SELF'], 'p.label', '', $param, '', $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans('Category'), $_SERVER['PHP_SELF'], 'c.nature_code', '', $param, '', $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans('PowerPlantSerialNumber'), $_SERVER['PHP_SELF'], 'c.serial_number', '', $param, '', $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans('PowerPlantCommissioningDate'), $_SERVER['PHP_SELF'], 'c.commissioning_date', '', $param, '', $sortfield, $sortorder);
+	print_liste_field_titre('', $_SERVER['PHP_SELF'], '', '', $param, 'class="center"', $sortfield, $sortorder);
+	print '</tr>';
+
+	if ($resql && $db->num_rows($resql) > 0) {
+		$num = min($db->num_rows($resql), $limit);
+		$i = 0;
+		while ($i < $num) {
+			$objline = $db->fetch_object($resql);
+			print '<tr class="oddeven">';
+			print '<td>'.dol_escape_htmltag($objline->product_ref).'</td>';
+			print '<td>'.dol_escape_htmltag($objline->product_label).'</td>';
+			print '<td>'.(isset($categories[(int) $objline->nature_code]) ? $categories[(int) $objline->nature_code] : '').'</td>';
+			print '<td>'.dol_escape_htmltag($objline->serial_number).'</td>';
+			print '<td>'.(!empty($objline->commissioning_date) ? dol_print_date($db->jdate($objline->commissioning_date), 'day') : '').'</td>';
+			print '<td class="center">';
+			if ($canedit) {
+				print '<a class="reposition" href="'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&action=delcomposition&lineid='.(int) $objline->rowid.'&token='.newToken().'">'.img_delete().'</a>';
+			}
+			print '</td>';
+			print '</tr>';
+			$i++;
+		}
+	} else {
+		print '<tr class="oddeven"><td colspan="6"><span class="opacitymedium">'.$langs->trans('None').'</span></td></tr>';
+	}
+
+	print '</table>';
+	print '</div>';
+	print '</form>';
+	print '</div>';
+	print dol_get_fiche_end();
+}
+
+llxFooter();
+$db->close();
