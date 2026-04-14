5223b7e7e12fa7cc725a2d9aac79e1efbf3528e6
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-15 00:13:35 +0200

    Masque le tab pvpanel via completeTabsHead selon extrafield

--- core/modules/modPowerPlantPV.class.php
+++ core/modules/modPowerPlantPV.class.php
@@ -120,7 +120,7 @@ class modPowerPlantPV extends DolibarrModules
 			),
 			// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context to 'all'
 			/* BEGIN MODULEBUILDER HOOKSCONTEXTS */
-			'hooks' => array(),
+			'hooks' => array('productcard'),
 			/* END MODULEBUILDER HOOKSCONTEXTS */
 			// Set this to 1 if features of module are opened to external users
 			'moduleforexternal' => 0,
