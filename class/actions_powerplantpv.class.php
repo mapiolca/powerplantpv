6eeea12ddd8d417cac0f35360e247bffc133f369
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-05-31 17:44:45 +0200

    fix button

--- class/actions_powerplantpv.class.php
+++ class/actions_powerplantpv.class.php
@@ -93,7 +93,7 @@ class ActionsPowerplantpv
 		}
 
 		$buttonid = 'powerplantpv-create-powerplant-'.$origin.'-'.((int) $object->id);
-		$dropdownhtml = dolGetButtonAction('', $langs->trans('CreatePowerPlantDropdown'), 'default', $url, $buttonid.'-dropdown', true, array('isDropDown' => true));
+		$dropdownhtml = '<a class="dropdown-item" href="'.dol_escape_htmltag($url).'" id="'.dol_escape_htmltag($buttonid.'-dropdown').'">'.dol_escape_htmltag($langs->trans('CreatePowerPlantDropdown')).'</a>';
 		print '<span id="'.dol_escape_htmltag($buttonid.'-holder').'" class="powerplantpv-create-powerplant-holder">';
 		print dolGetButtonAction($langs->trans('CreatePowerPlant'), $langs->trans('CreatePowerPlantDropdown'), 'default', $url, $buttonid, true);
 		print '</span>';
